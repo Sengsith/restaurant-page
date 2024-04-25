e2ca928b0f5f9a7c4a8c9a08a267a0acaba63010
Angelo Rosario <angelorosario10@gmail.com>
2024-04-25 13:27:58 -0700

    update index.js
    re-added h1 and p content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./style.css";
+
+const initializeContent = (() => {
+  const component = document.querySelector("#content");
+  const newH1 = document.createElement("h1");
+  const newP = document.createElement("p");
+
+  newH1.textContent = "Trails into the Pantry";
+  newP.textContent =
+    "Down below are various recipes that appear throught 'The Legend of Heroes' series!";
+
+  component.appendChild(newH1);
+  component.appendChild(newP);
+})();
