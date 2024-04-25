ed8360d23a82b176011967e5b4ed1e5798be34ef
Angelo Rosario <angelorosario10@gmail.com>
2024-04-25 13:38:45 -0700

    update index.js
    refactored intiial content into module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
 import "./style.css";
-
-const initializeContent = (() => {
-  const component = document.querySelector("#content");
-  const newH1 = document.createElement("h1");
-  const newP = document.createElement("p");
-
-  newH1.textContent = "Trails into the Pantry";
-  newP.textContent =
-    "Down below are various recipes that appear throught 'The Legend of Heroes' series!";
-
-  component.appendChild(newH1);
-  component.appendChild(newP);
-})();
+import initialzeContent from "./component/content/content";
