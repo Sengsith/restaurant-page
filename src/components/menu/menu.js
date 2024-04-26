4c7486deaa70682a0258c12a259f2b3de03ae1b7
Angelo Rosario <angelorosario10@gmail.com>
2024-04-26 16:12:10 -0700

    update menu.js menu.css
    menu layout and light styling

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,29 +1,42 @@
+import "./menu.css";
+
+import potOMeat from "../../assets/potomeat.png";
+import bouillabaisse from "../../assets/bouillabaisse.png";
+import chefCurry from "../../assets/curry.png";
+import wildVeggiePot from "../../assets/wildveggiepot.png";
+import jenisLunch from "../../assets/jenislunch.png";
+
 const initializeMenu = () => {
   const sitDownItems = [
     {
       name: "Pot O' Meat",
       desc: "This pot dish is a must try for meat lovers. Long live bacon!",
-      src: "https://static.wikia.nocookie.net/kiseki/images/a/a5/Item_7_%28Sora_Recipe%29.png/revision/latest?cb=20170103025235",
+      src: potOMeat,
+      alt: "meat-stew-in-clay-pot-on-portable-burner",
     },
     {
       name: "Bouillabaisse",
       desc: "A fabulous boiled seafood dish in rich bouillon-flavored stock.",
-      src: "https://static.wikia.nocookie.net/kiseki/images/9/9a/Item_11_%28Sora_Recipe%29.png/revision/latest?cb=20170103025237",
+      src: bouillabaisse,
+      alt: "seafood-dish-in-pot",
     },
     {
       name: "Chef's Curry",
       desc: "An appetizing authentic curry with a spicy aroma.",
-      src: "https://static.wikia.nocookie.net/kiseki/images/9/9a/Item_11_%28Sora_Recipe%29.png/revision/latest?cb=20170103025237",
+      src: chefCurry,
+      alt: "rice-plate-with-gravy-boat-and-water-glass",
     },
     {
       name: "Wild Veggie Pot",
       desc: "A mysterious glowing green pot dish from deep in the mountains.",
-      src: "https://static.wikia.nocookie.net/kiseki/images/d/d2/Item_4_%28Sora_Recipe%29.png/revision/latest?cb=20170103025234",
+      src: wildVeggiePot,
+      alt: "veggie-pot",
     },
     {
       name: "Jenis Lunch",
       desc: "A cheap, delicious school lunch. Nutritionally balanced.",
-      src: "https://static.wikia.nocookie.net/kiseki/images/5/50/Item_9_%28Sora_Recipe%29.png/revision/latest?cb=20170103025236",
+      src: jenisLunch,
+      alt: "chicken-steak-soup-dish",
     },
   ];
 
@@ -55,15 +68,22 @@ const initializeMenu = () => {
     newItem.classList.add("menu-item");
 
     const itemImg = new Image();
+    itemImg.classList.add("item-img");
+    const textContainer = document.createElement("div");
+    textContainer.classList.add("item-text");
     const itemName = document.createElement("p");
+    itemName.classList.add("item-name");
     const itemDesc = document.createElement("p");
+    itemDesc.classList.add("item-desc");
+    textContainer.appendChild(itemName);
+    textContainer.appendChild(itemDesc);
 
     itemImg.src = item.src;
+    itemImg.alt = item.alt;
     itemName.textContent = item.name;
     itemDesc.textContent = item.desc;
     newItem.appendChild(itemImg);
-    newItem.appendChild(itemName);
-    newItem.appendChild(itemDesc);
+    newItem.appendChild(textContainer);
 
     sitDownItemsContainer.appendChild(newItem);
   });
