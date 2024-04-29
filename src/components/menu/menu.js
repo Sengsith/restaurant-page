b9094d4b68159043d9237c11be95d47a158cf87a
Angelo Rosario <angelorosario10@gmail.com>
2024-04-29 10:32:11 -0700

    update menu.js
    Added drinks

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -5,6 +5,11 @@ import bouillabaisse from "../../assets/bouillabaisse.png";
 import chefCurry from "../../assets/curry.png";
 import wildVeggiePot from "../../assets/wildveggiepot.png";
 import jenisLunch from "../../assets/jenislunch.png";
+import mixedCocktail from "../../assets/mixedcocktail.png";
+import eggnog from "../../assets/eggnog.png";
+import fruitMilk from "../../assets/fruitmilk.png";
+import azeliaRose from "../../assets/azeliarose.png";
+import espresso from "../../assets/espresso.png";
 
 const initializeMenu = () => {
   const sitDownItems = [
@@ -40,10 +45,38 @@ const initializeMenu = () => {
     },
   ];
 
+  const drinkItems = [
+    {
+      name: "Mixed Cocktail",
+      desc: "The ultimate cocktail; crammed with condensed food extracts.",
+      src: mixedCocktail,
+    },
+    {
+      name: "Specialty Eggnog",
+      desc: "An Eastern world hot cocktail. Warms the body to the core.",
+      src: eggnog,
+    },
+    {
+      name: "Fruit Milk",
+      desc: "A fruit-flavored lactic treat. Perfect for after a bath.",
+      src: fruitMilk,
+    },
+    {
+      name: "Azelia Rose",
+      desc: "A refreshing cocktail made from a pungent fruit juice.",
+      src: azeliaRose,
+    },
+    {
+      name: "Rich Espresso",
+      desc: "A cup of coffee that will even wake a hibernating bear.",
+      src: espresso,
+    },
+  ];
+
   const content = document.querySelector("#content");
   content.textContent = "";
 
-  // Container for the entire menu
+  // Container for entire menu
   const newMenuContainer = document.createElement("div");
   newMenuContainer.classList.add("menu-container");
 
@@ -53,7 +86,7 @@ const initializeMenu = () => {
   newHeading.textContent = "Menu";
   newMenuContainer.appendChild(newHeading);
 
-  // Container for the sit down items
+  // Container for sit down items
   const sitDownItemsContainer = document.createElement("div");
   sitDownItemsContainer.classList.add("sit-down-items-container");
   newMenuContainer.appendChild(sitDownItemsContainer);
@@ -86,6 +119,41 @@ const initializeMenu = () => {
     newItem.appendChild(textContainer);
 
     sitDownItemsContainer.appendChild(newItem);
+  });
+
+  // Container for drinks
+  const drinkItemsContainer = document.createElement("div");
+  drinkItemsContainer.classList.add("drink-items-container");
+  newMenuContainer.appendChild(drinkItemsContainer);
+
+  const drinkItemsSubheading = document.createElement("h3");
+  drinkItemsSubheading.classList.add("subheading");
+  drinkItemsSubheading.textContent = "Drinks";
+  drinkItemsContainer.appendChild(drinkItemsSubheading);
+
+  drinkItems.forEach((item) => {
+    const newItem = document.createElement("div");
+    newItem.classList.add("menu-item");
+
+    const itemImg = new Image();
+    itemImg.classList.add("item-img");
+    const textContainer = document.createElement("div");
+    textContainer.classList.add("item-text");
+    const itemName = document.createElement("p");
+    itemName.classList.add("item-name");
+    const itemDesc = document.createElement("p");
+    itemDesc.classList.add("item-desc");
+    textContainer.appendChild(itemName);
+    textContainer.appendChild(itemDesc);
+
+    itemImg.src = item.src;
+    itemImg.alt = item.alt;
+    itemName.textContent = item.name;
+    itemDesc.textContent = item.desc;
+    newItem.appendChild(itemImg);
+    newItem.appendChild(textContainer);
+
+    drinkItemsContainer.appendChild(newItem);
   });
 
   content.appendChild(newMenuContainer);
